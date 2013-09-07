a6917bae7db416b44fae2b52b67cd10bcf532327
wilsonfreitas <wilson.freitas@gmail.com>
2013-09-07 20:20:37 -0300

    Create a separated spec to check whether or not the database exists

--- spec/sequelaSpec.js
+++ spec/sequelaSpec.js
@@ -13,24 +13,36 @@ describe("Sequela", function() {
         res = {};
     });
     
-    it("it should call the callback function to check database", function() {
-        var callback = jasmine.createSpy();
-        seq.check(null, callback);
-        waitsFor(function() {
-            return callback.callCount > 0;
+    describe("check", function() {
+        it("it should call the callback function to check database", function() {
+            var callback = jasmine.createSpy();
+            seq.check(null, callback);
+            waitsFor(function() {
+                return callback.callCount > 0;
+            });
+            runs(function() {
+                expect(callback).toHaveBeenCalled();
+            });
         });
-        runs(function() {
-            expect(callback).toHaveBeenCalled();
-        });
-    });
 
-    it("it should call check the database", function() {
-        seq.check('test.db', catchResponse());
-        waitsFor(function() {
-            return typeof res.value !== 'undefined';
+        it("it should check the database (it exists)", function() {
+            seq.check('test.db', catchResponse());
+            waitsFor(function() {
+                return typeof res.value !== 'undefined';
+            });
+            runs(function() {
+                expect(res.value).toEqual(true);
+            });
         });
-        runs(function() {
-            expect(res.value).toEqual(true);
+
+        it("it should check the database (it doesn't exist)", function() {
+            seq.check('doesntexist.db', catchResponse());
+            waitsFor(function() {
+                return typeof res.value !== 'undefined';
+            });
+            runs(function() {
+                expect(res.value).toEqual(false);
+            });
         });
     });
 
